b87c742905cf1c49ea8771ab7d78b6a5261fed51
ArtFED <temfedorenko@gmail.com>
2022-10-10 09:52:08 +0300

    add time & date

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,68 @@
-"use strict";
+'use strict';
 
-const time = document.querySelector(".time");
+window.addEventListener('DOMContentLoaded', () => {
+  // Clock and Calendar
 
-console.log(time);
+  const day = document.querySelector('.date');
+  const time = document.querySelector('.time');
+  const greeting = document.querySelector('.greeting');
 
-const date = new Date();
-const currentTime = date.toLocaleTimeString();
+  function showTime() {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime;
 
-console.log(currentTime);
-console.log(Date.now());
+    showDate();
+    showGreeting();
 
-time.textContent = currentTime;
+    setInterval(showTime, 1000);
+  }
+
+  showTime();
+
+  function showDate() {
+    const date = new Date();
+
+    const currentDate = date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+    });
+
+    day.textContent = currentDate;
+  }
+
+  // Greeting
+
+  function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if (hours < 6) {
+      timeOfDay = 'night';
+    } else if (hours < 12) {
+      timeOfDay = 'morning';
+    } else if (hours < 18) {
+      timeOfDay = 'afternoon';
+    } else if (hours < 24) {
+      timeOfDay = 'evening';
+    }
+
+    return timeOfDay;
+  }
+
+  function showGreeting() {
+    const timeOfDay = getTimeOfDay();
+    greeting.textContent = `Good ${timeOfDay},`;
+  }
+
+  // const nameInput = document.querySelector('.name');
+
+  // nameInput.addEventListener('change', () => {
+  //   const userName = nameInput.value;
+  //   console.log(userName);
+
+  //   // nameInput.value = userName;
+  // });
+});
