ddb959ff03357b9280b3cf2986fc647d5c624d62
ArtFED <temfedorenko@gmail.com>
2022-10-12 09:29:45 +0300

    add quotes

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
@@ -24,9 +20,9 @@ gulp.task('styles', function () {
 gulp.task('watch', function () {
   gulp.watch('src/css/*.css').on('all', gulp.parallel('styles'));
   gulp.watch('src/*.html').on('change', gulp.parallel('html'));
-  gulp.watch('src/js/**/*.js').on('change', gulp.parallel('scripts'));
-  gulp.watch('src/fonts/**/*').on('all', gulp.parallel('fonts'));
-  gulp.watch('src/icons/**/*').on('all', gulp.parallel('icons'));
+  gulp.watch('src/js/*.js').on('change', gulp.parallel('scripts'));
+  gulp.watch('src/fonts/*').on('all', gulp.parallel('fonts'));
+  gulp.watch('src/icons/*').on('all', gulp.parallel('icons'));
   gulp.watch('src/img/**/*').on('all', gulp.parallel('images'));
 });
 
@@ -38,15 +34,11 @@ gulp.task('html', function () {
 });
 
 gulp.task('scripts', function () {
-  return gulp.src('src/js/**/*.js').pipe(gulp.dest('dist/js')).pipe(browserSync.stream());
+  return gulp.src('src/js/*.js').pipe(gulp.dest('dist/js')).pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function () {
-  return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts')).pipe(browserSync.stream());
-});
-
-gulp.task('icons', function () {
-  return gulp.src('src/icons/**/*').pipe(gulp.dest('dist/icons')).pipe(browserSync.stream());
+  return gulp.src('src/fonts/*').pipe(gulp.dest('dist/fonts')).pipe(browserSync.stream());
 });
 
 gulp.task('images', function () {
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   // Clock and Calendar
 
-  const day = document.querySelector(".date");
-  const time = document.querySelector(".time");
-  const greeting = document.querySelector(".greeting");
-  const body = document.querySelector("body");
+  const day = document.querySelector('.date');
+  const time = document.querySelector('.time');
+  const greeting = document.querySelector('.greeting');
+  const body = document.querySelector('body');
 
   function showTime() {
     const date = new Date();
@@ -24,10 +24,10 @@ window.addEventListener("DOMContentLoaded", () => {
   function showDate() {
     const date = new Date();
 
-    const currentDate = date.toLocaleDateString("en-US", {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
+    const currentDate = date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
     });
 
     day.textContent = currentDate;
@@ -41,13 +41,13 @@ window.addEventListener("DOMContentLoaded", () => {
     let timeOfDay;
 
     if (hours < 6) {
-      timeOfDay = "night";
+      timeOfDay = 'night';
     } else if (hours < 12) {
-      timeOfDay = "morning";
+      timeOfDay = 'morning';
     } else if (hours < 18) {
-      timeOfDay = "afternoon";
+      timeOfDay = 'afternoon';
     } else if (hours < 24) {
-      timeOfDay = "evening";
+      timeOfDay = 'evening';
     }
 
     return timeOfDay;
@@ -60,21 +60,21 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // Save name
 
-  const nameInput = document.querySelector(".name");
+  const nameInput = document.querySelector('.name');
 
   function getLokalStorageName() {
-    if (localStorage.getItem("name")) {
-      nameInput.value = localStorage.getItem("name");
+    if (localStorage.getItem('name')) {
+      nameInput.value = localStorage.getItem('name');
     }
   }
 
   getLokalStorageName();
 
   function setLocalStorageName() {
-    localStorage.setItem("name", nameInput.value);
+    localStorage.setItem('name', nameInput.value);
   }
 
-  window.addEventListener("beforeunload", setLocalStorageName);
+  window.addEventListener('beforeunload', setLocalStorageName);
 
   //   Backgrond image from git
 
@@ -86,12 +86,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
   getRandomNum();
 
-  console.log(String(randomNum).padStart(2, "0"));
-
   function setBg() {
     const timeOfDay = getTimeOfDay();
-    const bgNum =
-      randomNum < 10 ? String(randomNum).padStart(2, "0") : randomNum;
+    const bgNum = randomNum < 10 ? String(randomNum).padStart(2, '0') : randomNum;
     const bgLink = `https://raw.githubusercontent.com/temfedorenko/moment-images/master/moment-images/${timeOfDay}/${bgNum}.jpg`;
 
     const img = new Image();
@@ -107,8 +104,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //   Slider
 
-  const prev = document.querySelector(".slide-prev");
-  const next = document.querySelector(".slide-next");
+  const prev = document.querySelector('.slide-prev');
+  const next = document.querySelector('.slide-next');
 
   function getSlidePrev() {
     randomNum -= 1;
@@ -130,58 +127,107 @@ window.addEventListener("DOMContentLoaded", () => {
     setBg();
   }
 
-  prev.addEventListener("click", getSlidePrev);
-  next.addEventListener("click", getSlideNext);
+  prev.addEventListener('click', getSlidePrev);
+  next.addEventListener('click', getSlideNext);
 
   // Weather
-  const city = document.querySelector(".city");
-  const weatherIcon = document.querySelector(".weather-icon");
-  const temperature = document.querySelector(".temperature");
-  const feelsLike = document.querySelector(".feels-like");
-  const weatherDescription = document.querySelector(".weather-description");
-  const humidity = document.querySelector(".humidity");
-  const wind = document.querySelector(".wind");
-  const weatherError = document.querySelector(".weather-error");
+  const city = document.querySelector('.city');
+  const weatherIcon = document.querySelector('.weather-icon');
+  const temperature = document.querySelector('.temperature');
+  const feelsLike = document.querySelector('.feels-like');
+  const weatherDescription = document.querySelector('.weather-description');
+  const humidity = document.querySelector('.humidity');
+  const wind = document.querySelector('.wind');
+  const weatherError = document.querySelector('.weather-error');
 
-  let userCity = "Kyiv";
-  city.value = "Kyiv";
-
-  city.addEventListener("change", () => {
-    userCity = city.value;
-    getWeather();
-  });
+  let userCity;
 
   function getLokalStorageCity() {
-    if (localStorage.getItem("city")) {
-      city.value = localStorage.getItem("city");
-      userCity = localStorage.getItem("city");
+    if (localStorage.getItem('city')) {
+      city.value = localStorage.getItem('city');
+      userCity = localStorage.getItem('city');
+      getWeather();
+    } else {
+      city.value = 'Kyiv';
+      userCity = 'Kyiv';
       getWeather();
     }
   }
+
+  city.addEventListener('change', () => {
+    userCity = city.value;
+    getWeather();
+  });
 
   async function getWeather() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&lang=en&appid=26385191cf2115e9533991a95ea5c8df&units=metric`;
     const response = await fetch(url);
     const data = await response.json();
 
-    weatherIcon.className = "weather-icon owf";
+    if (!response.ok) {
+      weatherError.textContent = 'city not found, please, try to enter your neerest city';
+      weatherIcon.className = 'weather-icon owf';
+      temperature.textContent = '';
+      feelsLike.textContent = '';
+      weatherDescription.textContent = '';
+      humidity.textContent = '';
+      wind.textContent = '';
+    }
+
+    weatherIcon.className = 'weather-icon owf';
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
     temperature.textContent = `${Math.round(data.main.temp)}°C`;
     feelsLike.textContent = `feels like: ${Math.round(data.main.feels_like)}°C`;
     weatherDescription.textContent = data.weather[0].description;
     humidity.textContent = `humidity: ${Math.round(data.main.humidity)}%`;
     wind.textContent = `wind speed: ${data.wind.speed.toFixed(1)} m/s`;
+    weatherError.textContent = '';
   }
 
-  getWeather();
+  // getWeather();
 
   //  Save city
 
   getLokalStorageCity();
 
   function setLocalStorageCity() {
-    localStorage.setItem("city", city.value);
+    localStorage.setItem('city', city.value);
   }
 
-  window.addEventListener("beforeunload", setLocalStorageCity);
+  window.addEventListener('beforeunload', setLocalStorageCity);
+
+  // Quotes
+
+  const quote = document.querySelector('.quote');
+  const author = document.querySelector('.author');
+  const quoteChanger = document.querySelector('.change-quote');
+  const quoteBody = document.querySelector('.quote-body');
+
+  const quoteImage = new Image();
+  quoteImage.src = '../img/loading-buffering.gif';
+  quoteImage.style.cssText = 'width: 24px; height: 24px;';
+
+  async function getQuotes() {
+    quote.insertAdjacentElement('afterbegin', quoteImage);
+    await fetch('https://type.fit/api/quotes', {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(data => {
+        const quoteNumber = Math.round(Math.random() * 1643);
+        quote.textContent = data[quoteNumber].text;
+        author.textContent = data[quoteNumber].author;
+      })
+      .catch(() => {
+        quote.textContent = 'Server error, please, try later';
+      });
+  }
+
+  getQuotes();
+
+  quoteChanger.addEventListener('click', () => {
+    quote.textContent = '';
+    author.textContent = '';
+    getQuotes();
+  });
 });
