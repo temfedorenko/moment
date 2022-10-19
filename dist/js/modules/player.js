ebeb253e1f74dbd9835ca8678fd57bf119cbb95c
ArtFED <temfedorenko@gmail.com>
2022-10-19 09:45:37 +0300

    finish settings styles

--- dist/js/modules/player.js
+++ dist/js/modules/player.js
@@ -1,22 +1,20 @@
-import playList from "./playList.js";
+import playList from './playList.js';
 
 // Simple Audio Player
 
 const playerWindow = () => {
-  const audio = new Audio(
-    "./sounds/red-hot-chili-peppers-dark-necessities.mp3"
-  );
-  const audioPrev = document.querySelector(".play-prev");
-  const audioNext = document.querySelector(".play-next");
-  const audioPlay = document.querySelector(".play");
-  const soundsList = document.querySelector(".play-list");
+  const audio = new Audio('./sounds/red-hot-chili-peppers-dark-necessities.mp3');
+  const audioPrev = document.querySelector('.play-prev');
+  const audioNext = document.querySelector('.play-next');
+  const audioPlay = document.querySelector('.play');
+  const soundsList = document.querySelector('.play-list');
 
   let playItem;
 
   function renderPlayList() {
     playList.forEach((item) => {
-      playItem = document.createElement("li");
-      playItem.classList.add("play-item");
+      playItem = document.createElement('li');
+      playItem.classList.add('play-item');
       playItem.textContent = item.title;
       soundsList.append(playItem);
     });
@@ -29,12 +27,12 @@ const playerWindow = () => {
 
   function deleteActiveClass() {
     Array.from(soundsList.children).forEach((item) => {
-      item.classList.remove("item-active");
+      item.classList.remove('item-active');
     });
   }
 
   function addActiveClass() {
-    soundsList.children[playNum].classList.add("item-active");
+    soundsList.children[playNum].classList.add('item-active');
   }
 
   function playAudio() {
@@ -43,14 +41,14 @@ const playerWindow = () => {
 
     if (isPlay) {
       audio.play();
-      audioPlay.classList.add("pause");
+      audioPlay.classList.add('pause');
     } else {
       audio.pause();
-      audioPlay.classList.remove("pause");
+      audioPlay.classList.remove('pause');
     }
   }
 
-  audioPlay.addEventListener("click", () => {
+  audioPlay.addEventListener('click', () => {
     isPlay = isPlay ? false : true;
     playAudio(playNum);
   });
@@ -65,11 +63,11 @@ const playerWindow = () => {
     audio.src = playList[playNum].src;
     isPlay = true;
     playAudio(playNum);
-    audioPlay.classList.add("pause");
+    audioPlay.classList.add('pause');
     trackTitle.textContent = playList[playNum].title;
   }
 
-  audioNext.addEventListener("click", nextSong);
+  audioNext.addEventListener('click', nextSong);
 
   function prevSong() {
     playNum -= 1;
@@ -81,23 +79,23 @@ const playerWindow = () => {
     audio.src = playList[playNum].src;
     isPlay = true;
     playAudio(playNum);
-    audioPlay.classList.add("pause");
+    audioPlay.classList.add('pause');
     trackTitle.textContent = playList[playNum].title;
   }
 
-  audioPrev.addEventListener("click", prevSong);
+  audioPrev.addEventListener('click', prevSong);
 
-  audio.addEventListener("ended", nextSong);
+  audio.addEventListener('ended', nextSong);
 
   // Advanced audioplayer
 
-  const trackTitle = document.querySelector(".track-title");
-  const currentTime = document.querySelector(".current-time");
-  const trackDuration = document.querySelector(".duration");
-  const timeline = document.querySelector(".timeline");
-  const progress = document.querySelector(".progress");
-  const volumeInput = document.querySelector(".volume-input");
-  const volumeBtn = document.querySelector(".volume-btn");
+  const trackTitle = document.querySelector('.track-title');
+  const currentTime = document.querySelector('.current-time');
+  const trackDuration = document.querySelector('.duration');
+  const timeline = document.querySelector('.timeline');
+  const progress = document.querySelector('.progress');
+  const volumeInput = document.querySelector('.volume-input');
+  const volumeBtn = document.querySelector('.volume-btn');
 
   // Progress Bar
 
@@ -108,7 +106,7 @@ const playerWindow = () => {
     trackDuration.textContent = playList[playNum].duration;
   }
 
-  audio.addEventListener("timeupdate", updateProgress);
+  audio.addEventListener('timeupdate', updateProgress);
 
   function setProgress(e) {
     const timelineWidth = e.currentTarget.clientWidth;
@@ -117,7 +115,7 @@ const playerWindow = () => {
     audio.currentTime = (clickX / timelineWidth) * audio.duration;
   }
 
-  timeline.addEventListener("click", setProgress);
+  timeline.addEventListener('click', setProgress);
 
   // setInterval(updateProgress, 500);
 
@@ -125,7 +123,7 @@ const playerWindow = () => {
     let min = Math.floor(seconds / 60);
     let sec = Math.floor(seconds % 60);
 
-    min = min === "NaN:NaN" ? `0:00` : min;
+    min = min === 'NaN:NaN' ? `0:00` : min;
     sec = sec < 10 ? `0${sec}` : sec;
 
     return `${min}:${sec}`;
@@ -133,22 +131,22 @@ const playerWindow = () => {
 
   //  -----Volume-----
 
-  volumeInput.addEventListener("input", () => {
+  volumeInput.addEventListener('input', () => {
     audio.volume = volumeInput.value;
 
     if (volumeInput.value > 0) {
-      volumeBtn.classList.remove("no-volume");
+      volumeBtn.classList.remove('no-volume');
     }
 
     if (volumeInput.value == 0) {
-      volumeBtn.classList.add("no-volume");
+      volumeBtn.classList.add('no-volume');
     }
   });
 
-  volumeBtn.addEventListener("click", () => {
-    volumeBtn.classList.toggle("no-volume");
+  volumeBtn.addEventListener('click', () => {
+    volumeBtn.classList.toggle('no-volume');
 
-    if (volumeBtn.classList.contains("no-volume")) {
+    if (volumeBtn.classList.contains('no-volume')) {
       audio.volume = 0;
       volumeInput.value = 0;
     } else {
--- dist/js/modules/quotes.js
+++ dist/js/modules/quotes.js
@@ -1,41 +1,39 @@
-import { language } from "./translator.js";
+import { language } from './translator.js';
 
-const quote = document.querySelector(".quote");
-const author = document.querySelector(".author");
-const quoteChanger = document.querySelector(".change-quote");
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const quoteChanger = document.querySelector('.change-quote');
 
 const quoteImage = new Image();
-quoteImage.src = "./img/loading-buffering.gif";
-quoteImage.style.cssText = "width: 24px; height: 24px;";
+quoteImage.src = './img/loading-buffering.gif';
+quoteImage.style.cssText = 'width: 24px; height: 24px;';
 
 export function getQuotes() {
-  quote.textContent = "";
-  author.textContent = "";
-  quote.insertAdjacentElement("afterbegin", quoteImage);
+  quote.textContent = '';
+  author.textContent = '';
+  quote.insertAdjacentElement('afterbegin', quoteImage);
 
-  const url =
-    language === "en" ? "https://type.fit/api/quotes" : "../quotes.json";
+  const url = language === 'en' ? 'https://type.fit/api/quotes' : '../quotes.json';
   fetch(url, {
-    method: "GET",
+    method: 'GET',
   })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.length);
+    .then(response => response.json())
+    .then(data => {
       const quoteNumber = Math.floor(Math.random() * data.length);
       quote.textContent = data[quoteNumber].text;
       author.textContent = data[quoteNumber].author;
     })
     .catch(() => {
-      quote.textContent = "Server error, please, try later";
+      quote.textContent = 'Server error, please, try later';
     });
 }
 
 // getQuotes();
 
 export function refreshQuote() {
-  quote.textContent = "";
-  author.textContent = "";
+  quote.textContent = '';
+  author.textContent = '';
   getQuotes();
 }
 
-quoteChanger.addEventListener("click", refreshQuote);
+quoteChanger.addEventListener('click', refreshQuote);
